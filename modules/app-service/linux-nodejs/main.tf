cca62b3c707b1ae45405111ebff3602f9ea68fef
Dmitry Grinko <dmitry.grinko@sogeti.com>
2023-04-19 12:38:48 -0700

    init

--- modules/app-service/linux-nodejs/main.tf
+++ modules/app-service/linux-nodejs/main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_linux_web_app" "example" {
+resource "azurerm_linux_web_app" "app" {
   name                = var.app_name
   location            = var.resource_group_location
   resource_group_name = var.resource_group_name
